d749e56c2ee9ab779b39a4f271b0bc8152b60983
Dawid Kotowski <dkotowsk@uni-muenster.de>
2025-09-20 21:45:50 +0200

    reworked into dimension framework and reduced data load

--- examples/ex01/module_training/lin_dod_dl_train_ex01.py
+++ examples/ex01/module_training/lin_dod_dl_train_ex01.py
@@ -1,20 +1,22 @@
 import torch
-from master_project_1 import dod_dl_rom as dr
+from master_project_1 import reduced_order_models as dr
 import numpy as np
 
 
 # Usage example
 N_h = 5101
 N_A = 64
-nt = 10
-diameter = 0.02
+N = 64
+N_prime = 16
 n = 4
+Nt = 10
+diameter = 0.02
 parameter_mu_dim = 1
 parameter_nu_dim = 1
 # linear DOD-DL-ROM
 preprocess_dim = 2
 lin_m = 4
-lin_dod_structure = [128, 64]
+lin_dod_structure = [32, 16]
 lin_phi_n_structure = [16, 8]
 # POD-DL-ROM
 pod_coeff_ae_structure = [32, 16, 8]
@@ -31,19 +33,18 @@ stat_dod_structure = [128, 64]
 stat_phi_n_structure = [16, 8]
 
 # Training Example
-generalepochs = 500
+generalepochs = 50
 generalrestarts = 10
 generalpatience = 3
 
 # Fetch Training and Validation set
-train_valid_data = dr.FetchReducedTrainAndValidSet(0.8, 'ex01')
-stat_train_valid_data = dr.StatFetchReducedTrainAndValidSet(0.8, 'ex01')
+train_valid_data = dr.FetchTrainAndValidSet(0.8, 'ex01', 'N_A_reduced')
 
 # Initialize the DOD model
-DOD_DL_model = dr.DOD_DL(preprocess_dim, parameter_mu_dim, lin_dod_structure, n, N_A)
+DOD_DL_model = dr.DOD_DL(preprocess_dim, parameter_mu_dim, lin_dod_structure, N_prime, N_A)
 
 # Initialize the DOD trainer
-DOD_DL_trainer = dr.DOD_DL_Trainer(DOD_DL_model, train_valid_data, N_A, 'ex01',
+DOD_DL_trainer = dr.DOD_DL_Trainer(DOD_DL_model, train_valid_data,
                                    generalepochs,generalrestarts, learning_rate=1e-4, 
                                    batch_size=128, patience=generalpatience)
 
@@ -52,14 +53,14 @@ best_loss = DOD_DL_trainer.train()
 print(f"Best validation loss: {best_loss}")
 
 # Initialize the Coefficient Finding model
-DOD_DL_coeff_model = dr.Coeff_DOD_DL(parameter_mu_dim, parameter_nu_dim, lin_m, n, lin_phi_n_structure)
+DOD_DL_coeff_model = dr.Coeff_DOD_DL(parameter_mu_dim, parameter_nu_dim, lin_m, N_prime, lin_phi_n_structure)
 
 # Initialize the Coefficient Finding trainer
 DOD_DL_coeff_trainer = dr.Coeff_DOD_DL_Trainer(N_A, DOD_DL_model, DOD_DL_coeff_model,
-                                   train_valid_data, 'ex01', 
+                                   train_valid_data,
                                    generalepochs, generalrestarts, learning_rate=1e-3, 
                                    batch_size=128, patience=generalpatience)
 
 # Save the Models to a file
-torch.save(DOD_DL_model.state_dict(), '/home/sereom/Documents/University/Studies/Mathe/Wissenschaftliche Arbeiten/Master/Masterarbeit Ohlberger/Programming/master-project-1/examples/ex01/state_dicts/DOD_Module.pth')
-torch.save(DOD_DL_coeff_model.state_dict(), '/home/sereom/Documents/University/Studies/Mathe/Wissenschaftliche Arbeiten/Master/Masterarbeit Ohlberger/Programming/master-project-1/examples/ex01/state_dicts/DOD_Coefficient_Module.pth')
+torch.save(DOD_DL_model.state_dict(), 'examples/ex01/state_dicts/DOD_Module.pth')
+torch.save(DOD_DL_coeff_model.state_dict(), 'examples/ex01/state_dicts/DOD_Coefficient_Module.pth')
--- examples/ex01/module_training/pod_dl_train_ex01.py
+++ examples/ex01/module_training/pod_dl_train_ex01.py
@@ -1,20 +1,22 @@
 import torch
-from master_project_1 import dod_dl_rom as dr
+from master_project_1 import reduced_order_models as dr
 import numpy as np
 
 
 # Usage example
 N_h = 5101
 N_A = 64
-nt = 10
-diameter = 0.02
+N = 64
+N_prime = 16
 n = 4
+Nt = 10
+diameter = 0.02
 parameter_mu_dim = 1
 parameter_nu_dim = 1
 # linear DOD-DL-ROM
 preprocess_dim = 2
 lin_m = 4
-lin_dod_structure = [128, 64]
+lin_dod_structure = [32, 16]
 lin_phi_n_structure = [16, 8]
 # POD-DL-ROM
 pod_coeff_ae_structure = [32, 16, 8]
@@ -31,27 +33,26 @@ stat_dod_structure = [128, 64]
 stat_phi_n_structure = [16, 8]
 
 # Training Example
-generalepochs = 500
+generalepochs = 50
 generalrestarts = 10
 generalpatience = 3
 
 # Fetch Training and Validation set
-train_valid_data = dr.FetchReducedTrainAndValidSet(0.8, 'ex01')
-stat_train_valid_data = dr.StatFetchReducedTrainAndValidSet(0.8, 'ex01')
+train_valid_data = dr.FetchTrainAndValidSet(0.8, 'ex01', 'N_reduced')
 
 # Initialize the POD DL ROM model
-output = int(np.sqrt(N_A))
+output = int(np.sqrt(N))
 pod_num_layers = 0
 while (output - int(np.sqrt(n)) > pod_lin_dim_ae):
     output = int(np.floor((output + 2*pod_padding - pod_kernel) / pod_stride) + 1)
     pod_num_layers += 1
-En_model = dr.Encoder(N_A, pod_in_channels, pod_hidden_channels, n, pod_num_layers, pod_kernel, pod_stride, pod_padding)
-De_model = dr.Decoder(N_A, pod_in_channels, pod_hidden_channels, n, pod_num_layers, pod_kernel, pod_stride, pod_padding)
+En_model = dr.Encoder(N, pod_in_channels, pod_hidden_channels, n, pod_num_layers, pod_kernel, pod_stride, pod_padding)
+De_model = dr.Decoder(N, pod_in_channels, pod_hidden_channels, n, pod_num_layers, pod_kernel, pod_stride, pod_padding)
 POD_DL_coeff_model = dr.Coeff_AE(parameter_mu_dim, parameter_nu_dim, n, pod_coeff_ae_structure)
 
 # Initialize the AE Coefficient Finding trainer
 POD_DL_coeff_trainer = dr.POD_DL_Trainer(POD_DL_coeff_model, En_model, De_model,
-                                    train_valid_data, 'ex01', 0.999,
+                                    train_valid_data, 0.999,
                                     generalepochs, generalrestarts, learning_rate=1e-2, 
                                     batch_size=128, patience=generalpatience)
 
@@ -64,4 +65,4 @@ torch.save({
     'encoder': En_model.state_dict(),
     'decoder': De_model.state_dict(),
     'coeff_model': POD_DL_coeff_model.state_dict(),
-}, '/home/sereom/Documents/University/Studies/Mathe/Wissenschaftliche Arbeiten/Master/Masterarbeit Ohlberger/Programming/master-project-1/examples/ex01/state_dicts/POD_DL_Module.pth')
+}, 'examples/ex01/state_dicts/POD_DL_Module.pth')
